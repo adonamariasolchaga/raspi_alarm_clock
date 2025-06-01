5f588605cbe3f0f7972db5b6e5bfd96d7ce63953
Alvaro Donamaria <donamin99@gmail.com>
2025-06-01 11:26:38 +0200

    Project set up

--- README.md
+++
@@ -1,3 +0,0 @@
-### Compilation steps
-1. `cd` to build directory
-2. Execute `make` command
--- build/_deps/picotool-build/data_locs.cpp
+++
@@ -1,7 +0,0 @@
-
-#include <vector>
-#include <string>
-
-std::vector<std::string> data_locs = {
-    "./","/home/alvaro/Documentos/repos/raspi_alarm_clock/build/_deps/picotool/"
-};
